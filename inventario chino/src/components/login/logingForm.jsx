6a4b9ccd4acc54c262eef5f4c76830d88c8e968b
W-Alexander <alexanderaguilar726@gmail.com>
2024-07-09 23:45:56 -0600

    Redirección de login a home.

--- inventario chino/src/components/login/logingForm.jsx
+++ inventario chino/src/components/login/logingForm.jsx
@@ -1,8 +1,11 @@
-import { useState,useNavigate } from "react";
-import { obtenerInformacionDatos } from "../services/llamados";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { obtenerInformacionDatos } from "../../services/llamados";
+import "./loginForm.css";
 
 const logingForm = () => {
   //creamos una función que va a validar que el usuario que se vaya a logear, exista y en caso positivo será redirigido al mainpage.
+  const navigate = useNavigate();
   const [correo, setCorreo] = useState("");
   const [contrasena, setContresena] = useState("");
 
@@ -20,6 +23,7 @@ const logingForm = () => {
       );
       if (usuariosRegistrado) {
         alert("Datos correctos, en breve será dirigido a la página de inicio");
+        navigate("/home");
       } else {
         alert("Hubo un error con los datos, intentelo de nuevo");
       }
@@ -29,27 +33,25 @@ const logingForm = () => {
   };
 
   return (
-    <div>
-      <div>
-        <p>Login</p>
-      </div>
-      <p>Correo</p>
+    <div className="div-inputs-login">
+      <h1 className="titulo">Login</h1>
       <input //le damos a cada input las propiedadades necesarias para utilizarlas y al botón le enviamos la función.
         onChange={(e) => setCorreo(e.target.value)}
         type="text"
         placeholder="Ingrese correo"
         name="correo"
         value={correo}
+        className="inputs-login"
       />
-      <p>Contraseña</p>
       <input
         onChange={(e) => setContresena(e.target.value)}
         type="password"
         placeholder="Ingrese Contraseña"
         name="contrasena"
         value={contrasena}
+        className="inputs-login"
       />
-      <button onClick={obtenerDatos}>Login</button>
+      <button className="boton" onClick={obtenerDatos}>Login</button>
     </div>
   );
 };
